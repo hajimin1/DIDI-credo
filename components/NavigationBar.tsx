fb224d9e77ba1f5b2ecf299a1c3cd2bf765a5275
Jimin Ha <smjmjh26@naver.com>
2024-08-20 13:41:57 +0900

    separate camera component from navigationbar component

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,38 +1,10 @@
 import {useState} from 'react';
-import {StyleSheet} from 'react-native';
 import {BottomNavigation, Text} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {
-  Camera,
-  useCameraDevice,
-  useCameraPermission,
-  useCodeScanner,
-} from 'react-native-vision-camera';
+import QrCamera from './QrCamera';
 
 export default function NavigationBar() {
-  const {hasPermission, requestPermission} = useCameraPermission();
-  const device = useCameraDevice('back');
-  const codeScanner = useCodeScanner({
-    codeTypes: ['qr', 'ean-13'],
-    onCodeScanned: codes => {
-      console.log(`Scanned ${codes.length} codes!`);
-      console.log(`Scanned ${codes}`);
-    },
-  });
-  requestPermission();
-  if (!hasPermission) return <Text>No Permission</Text>;
-  if (device == null) return <Text>No Camera</Text>;
-
-  const QrRoute = () => (
-    <>
-      <Camera
-        style={StyleSheet.absoluteFill}
-        codeScanner={codeScanner}
-        device={device}
-        isActive={true}
-      />
-    </>
-  );
+  const QrRoute = () => <QrCamera />;
 
   const VoteRoute = () => <Text>vote</Text>;
 
---
+++ components/QrCamera.tsx
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+import {Text} from 'react-native-paper';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useCodeScanner,
+} from 'react-native-vision-camera';
+
+export default function QrCamera() {
+  const {hasPermission, requestPermission} = useCameraPermission();
+  const device = useCameraDevice('back');
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: codes => {
+      console.log(`Scanned ${codes.length} codes!`);
+      console.log(`Scanned ${codes}`);
+    },
+  });
+  requestPermission();
+  if (!hasPermission) return <Text>No Permission</Text>;
+  if (device == null) return <Text>No Camera</Text>;
+  return (
+    <>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        codeScanner={codeScanner}
+        device={device}
+        isActive={true}
+      />
+    </>
+  );
+}
